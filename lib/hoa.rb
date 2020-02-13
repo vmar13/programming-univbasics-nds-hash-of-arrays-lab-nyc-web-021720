59420d0f5c3bf1302e1a4e0162f6cdde9dcbb8f1
Vanessa Martinez <vmar76@gmail.com>
2020-02-13 04:19:04 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,8 +6,11 @@ BASE_HOA = {
 
 
 def add_character(show, name)
-  [show] << name
+  [show] << nam
 end
+
+(:third_earthers, "Snarf")).to eq(["Lion-O", "Cheetara", "Mumm-Ra (the ever-living)", "Snarf"])
+    end
 
 # Write your implementation here
   # Should return the array of the 'show' argument
